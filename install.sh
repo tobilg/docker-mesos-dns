d38c8c159699ad31755e39be988ade3a0afb1fa7
tobilg <tobilg@users.noreply.github.com>
2015-12-14 10:13:43 +0100

    Update to Mesos DNS v0.5.1

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ apk update
 apk add wget gzip sed bash
 
 # Install Mesos DNS
-wget https://github.com/mesosphere/mesos-dns/releases/download/$MESOS_DNS_VERSION/$MESOS_DNS_FILENAME.gz && \
-    gzip -d $MESOS_DNS_FILENAME.gz && \
+wget https://github.com/mesosphere/mesos-dns/releases/download/$MESOS_DNS_VERSION/$MESOS_DNS_FILENAME && \
     mkdir -p $MESOS_DNS_PATH && \
     mv $MESOS_DNS_FILENAME $MESOS_DNS_PATH/mesos-dns && \
     chmod +x $MESOS_DNS_PATH/mesos-dns
